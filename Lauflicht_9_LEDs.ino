0d71be6984398d015fa8f31faa9cc6d3ee8b42a8
jakob-uno <53879561+jakob-uno@users.noreply.github.com>
2019-08-07 22:46:36 +0200

    Add files via upload

---
+++ Lauflicht_9_LEDs.ino
@@ -0,0 +1,327 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(1,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,LOW);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
+
+  delay(600);
+
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
+
+  delay(600);
+
+ 
+}
